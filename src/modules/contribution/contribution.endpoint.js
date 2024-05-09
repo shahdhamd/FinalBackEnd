8e63e1e7eeaab631ed11d67538add065fbc34bd0
shahdhamd <113027252+shahdhamd@users.noreply.github.com>
2024-05-09 17:04:02 +0300

    Update contribution.endpoint.js

--- src/modules/contribution/contribution.endpoint.js
+++ src/modules/contribution/contribution.endpoint.js
@@ -1,8 +1,9 @@
 import { roles } from "../../services/roles.js";
 
 export const endpoint={
-    getAll:[roles.Admin],
-    update:[roles.Admin,roles.User],
-    createContribution:[roles.Admin,roles.User],
-    delete:[roles.User,roles.Admin]
+    createHerb:[roles.Admin],
+    delete:[roles.Admin],
+    update:[roles.Admin],
+    search:[roles.User,roles.Admin],
+    getAllHerb:[roles.User,roles.Admin],
 }
